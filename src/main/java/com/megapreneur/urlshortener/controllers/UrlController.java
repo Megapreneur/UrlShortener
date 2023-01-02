268c9e0e6af2d501116de92cbe0fd79dd7d0ae80
Megapreneur <ademolamegbabi@gmail.com>
2023-01-02 21:40:34 +0100

    still on controller

--- src/main/java/com/megapreneur/urlshortener/controllers/UrlController.java
+++ src/main/java/com/megapreneur/urlshortener/controllers/UrlController.java
@@ -2,14 +2,12 @@ package com.megapreneur.urlshortener.controllers;
 
 import com.megapreneur.urlshortener.dtos.GenerateUrlRequest;
 import com.megapreneur.urlshortener.dtos.GenerateUrlResponse;
+import com.megapreneur.urlshortener.exceptions.UrlNotFoundException;
 import com.megapreneur.urlshortener.models.Urls;
 import com.megapreneur.urlshortener.service.UrlsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/url")
@@ -21,6 +19,14 @@ public class UrlController {
     public ResponseEntity<?> generateShortenedUrl(@RequestBody GenerateUrlRequest request){
         GenerateUrlResponse url = urlsService.generateShortUrl(request);
         return ResponseEntity.ok(url.getMessage());
+    }
+    @DeleteMapping("/{originalUrl}")
+    public void deleteOriginalUrl(@PathVariable String originalUrl) throws UrlNotFoundException{
+        urlsService.deleteUrlUsingOriginalUrl(originalUrl);
+    }
+    @DeleteMapping("/{shortenedUrl}")
+    public void deleteShortenedUrl(@PathVariable String shortenedUrl) throws UrlNotFoundException{
+        urlsService.deleteUrlUsingShortenedUrl(shortenedUrl);
     }
 
 }
